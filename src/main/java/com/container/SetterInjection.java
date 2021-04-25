506b50db954770c49f3374e3af0fcb188b190853
black3279 <30133279@naver.com>
2021-04-26 00:52:54 +0900

    [20210426] Spring 5 Study Commit

---
+++ src/main/java/com/container/SetterInjection.java
@@ -0,0 +1,15 @@
+package com.container;
+
+public class SetterInjection {
+
+    private Dependency dependency;
+
+    public void setDependency(Dependency dependency) {
+        this.dependency = dependency;
+    }
+
+    @Override
+    public String toString(){
+        return dependency.toString();
+    }
+}
