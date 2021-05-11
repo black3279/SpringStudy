f34b527deda19b30e8f3fba69f04d4586cc7f4a8
black3279 <30133279@naver.com>
2021-05-11 23:10:32 +0900

    [20210511] Spring 5 Study Commit

---
+++ src/main/java/com/collection/LyricHolder.java
@@ -0,0 +1,10 @@
+package com.collection;
+
+public class LyricHolder {
+    private String value = "'You be the DJ, I'll be the driver'";
+
+    @Override
+    public String toString(){
+        return "LyricHolder: { " + value + " }";
+    }
+}
